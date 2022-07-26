c32269d75d5b7d59337083ea341e698fa5279594
Veljko Stojadinovic <vstojadinovic.996@gmail.com>
2022-07-26 15:07:28 +0200

    comments, posts components

--- components/Postcard.jsx
+++ components/Postcard.jsx
@@ -17,7 +17,7 @@ const PostCard = ({post}) => {
                 </Link>
             </h1>
             <div className='block text-center items-center justify-center mb-8 w-full'>
-                <div className='flex items-center justify-center mb-4 lg:mb-2 w-full lg:w-auto mr-8'>
+                <div className='flex items-center justify-center mb-2 lg:mb-2 w-full lg:w-auto mr-8'>
                     <img
                         alt = {post.author.name}
                         height= '30px'
@@ -25,9 +25,9 @@ const PostCard = ({post}) => {
                         className='align-middle rounded-full'
                         src={post.author.photo.url}
                     />
-                    <p className="inline align-middle text-gray-700 ml-2 mb-4 font-medium text-lg">{post.author.name}</p>
+                    <p className="inline align-middle text-gray-700 ml-2 mb-2 font-medium text-lg">{post.author.name}</p>
                 </div>
-                <div className='font-medium text-gray-700 mb-4  '>
+                <div className='font-medium text-gray-700 mb-4 mr-4 '>
                     <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 inline mr-2 text-pink-500" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z" />
                     </svg>
--- services/index.js
+++ services/index.js
@@ -36,7 +36,42 @@ export const getPosts = async () => {
     const result = await request(graphqlAPI,query)
 
     return result.postsConnection.edges
-}
+};
+
+export const getPostDetails = async (slug) => {
+    const query = gql `
+        query GetPostDetails($slug: String!) {
+            post(where:{slug: $slug}) {
+                    author {
+                    bio
+                    name
+                    id
+                    photo {
+                        url
+                    }
+                }
+                createdAt
+                slug
+                title
+                excerpt
+                featuredImage {
+                    url
+                }
+                categories {
+                    name
+                    slug
+                }
+                content{
+                    raw
+                }
+            }
+        }`
+
+    const result = await request(graphqlAPI,query, {slug});
+
+    return result.post;
+};
+
 
 export const getRecentPosts = async () => {
     const query = gql `
@@ -58,7 +93,7 @@ export const getRecentPosts = async () => {
 
     return result.posts
 
-}
+};
 
 export const getSimilarPosts = async (categories, slug) => {
   const query = gql`
@@ -79,4 +114,48 @@ export const getSimilarPosts = async (categories, slug) => {
   const result = await request(graphqlAPI, query, { slug, categories });
 
   return result.posts;
+};
+
+
+
+export const getCategories = async () => {
+    const query = gql `
+    query GetCategories {
+        categories { 
+            name
+            slug
+        }
+    }`;
+
+    const result = await request(graphqlAPI,query)
+    
+    return result.categories
+};
+
+
+export const submitComment = async (obj) => {
+    const result = await fetch('/api/comments', {
+        method: 'POST',
+        headers: {
+            'Content-Type':'application/json'
+        },
+        body: JSON.stringify(obj),
+    });
+
+    return result.json();
+}
+
+export const getComments = async (slug) => {
+  const query = gql`
+    query GetComments($slug:String!) {
+      comments(where: {post: {slug:$slug}}){
+        name
+        createdAt
+        comment
+      }
+    }
+  `
+  const result = await request(graphqlAPI, query, { slug });
+
+  return result.comments;
 };
