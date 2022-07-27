7096b933e8b61a9b3d54fd4ed3964a1ae163f42c
Veljko Stojadinovic <vstojadinovic.996@gmail.com>
2022-07-27 13:22:43 +0200

    css update

--- components/PostDetail.jsx
+++ components/PostDetail.jsx
@@ -61,19 +61,19 @@ const PostDetail = ({ post }) => {
 
   return (
     <div className="bg-white shadow-lg rounded-lg lg:p-8 pb-12 mb-8">
-      <div className="relative overflow-hidden shadow-md mb-6">
+      <div className="relative overflow-hidden p-4 mb-6">
         <img
           src={post.featuredImage.url}
           alt={post.title}
           className="object-top h-full w-full rounded-t-lg"
         />
         <div className="px-4 lg:px-0">
-          <div className="flex items-center mb-8 w-full">
-            <div className="flex items-center mb-2 lg:mb-2 w-full lg:w-auto mr-8">
+          <div className="flex items-center p-2 mb-8 w-full">
+            <div className="flex items-center mb-2 lg:mb-4 w-full lg:w-auto mr-8">
               <img
                 alt={post.author.name}
-                height="30px"
-                width="30px"
+                height="40px"
+                width="40px"
                 className="align-middle rounded-full"
                 src={post.author.photo.url}
               />
@@ -84,7 +84,7 @@ const PostDetail = ({ post }) => {
             <div className="font-medium text-gray-700 mb-4 mr-4 ">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
-                className="h-6 w-6 inline mr-2 text-pink-500"
+                className="h-8 w-8 inline mt-2 mr-2 text-pink-500"
                 fill="none"
                 viewBox="0 0 24 24"
                 stroke="currentColor"
@@ -96,11 +96,15 @@ const PostDetail = ({ post }) => {
                   d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z"
                 />
               </svg>
-              <span>{moment(post.createdAt).format("MMM DD, YYYY")}</span>
+              <span className="inline mt-4">
+                {moment(post.createdAt).format("MMM DD, YYYY")}
+              </span>
             </div>
           </div>
         </div>
-        <h1 className="mb-8 text-3xl font-semibold">{post.title}</h1>
+        <h1 className="mb-8 text-3xl font-semibold text-center">
+          {post.title}
+        </h1>
         {post.content.raw.children.map((typeObj, index) => {
           const children = typeObj.children.map((item, itemindex) =>
             getContentFragment(itemindex, item.text, item)
