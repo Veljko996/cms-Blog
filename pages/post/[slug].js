0a8432a92c75b29f354bfe8ae2cbc5eb3bbef310
Veljko Stojadinovic <vstojadinovic.996@gmail.com>
2022-07-27 10:43:56 +0200

    carousel

--- pages/post/[slug].js
+++ pages/post/[slug].js
@@ -1,9 +1,16 @@
 import React from "react";
 import { getPosts,getPostDetails } from "../../services";
-import {PostDetail, Categories, PostWidget, Author, Comments,CommentsForm } from "../../components"
+import {PostDetail, Categories, PostWidget, Author, Comments,CommentsForm, Loader} from "../../components"
+import { useRouter } from "next/router";
 
 
 const PostDetails = ({post}) => {
+
+  const router = useRouter()
+
+  if(router.isFallback){
+    return <Loader/>
+  }
 
     return (
         <div className="container mx-auto px-10 mb-8">
